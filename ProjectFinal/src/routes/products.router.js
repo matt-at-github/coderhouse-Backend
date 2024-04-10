9f4feecd7c044be304144ae953cd1c1815e37c7a
Matías Moresi <MatiasMoresi@getyooz.com>
2024-04-10 11:51:30 -0300

    dev

--- ProjectFinal/src/routes/products.router.js
+++ ProjectFinal/src/routes/products.router.js
@@ -7,7 +7,11 @@ const productController = new ProductController();
 /*
  * La única observación que puedo marcarte es que en el archivo products.router.js 
  * falta incluir los endpoints que realizan las 
- * solicitudes POST, PUT y DELETE asociadas a las productos. Estos endpoints no podrán faltar en la 3ra preentrega. 
+ * solicitudes POST, PUT y DELETE asociadas a las productos.
+ * Estos endpoints no podrán faltar en la 3ra preentrega. 
+ * 
+ * Fede: Estos endpoints se encuentran en ProjectFinal\src\routes\API\api.products.router.js
+ * De ser necesario que esten todos en el mismo router los muevo, pero quería tener los endpoints separados.
  */
 
 // // Get all products / Get Products by query
