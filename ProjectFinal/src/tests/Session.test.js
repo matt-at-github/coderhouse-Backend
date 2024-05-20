30d2dbe5b56a5e1ef7cb6cd0c75e8744068dee15
Matías Moresi <MatiasMoresi@getyooz.com>
2024-05-20 16:23:58 -0300

    test-Users

---
+++ ProjectFinal/src/tests/Session.test.js
@@ -0,0 +1,41 @@
+const { mongoConnectionConfig } = require('../config/config.js');
+const mongoose = require('mongoose');
+mongoose.connect(mongoConnectionConfig.url);
+
+const supertest = require('supertest');
+const mocha = require('mocha');
+const { expect } = require('chai');
+
+const describe = mocha.describe;
+const it = mocha.it;
+
+const requester = supertest('http://localhost:8080');
+
+describe('Session testing', function () {
+
+  it('Al iniciar sesión, debe retornar el token', async function () {
+
+    const loginCredentials = { email: 'matiasnicolasmoresi@gmail.com', password: '1234' };
+    const response = await requester.post('/users/login').send(loginCredentials);
+
+    const cookieName = response.headers['set-cookie'][0].split('=')[0];
+    console.log(cookieName);
+    expect(cookieName).to.be.equal('OnlyShopToken');
+  });
+
+  it('iniciar sesión con mail inválido', async function () {
+
+    const loginCredentials = { email: 'matiasnicolasmoresi@gmail', password: '123' };
+    const response = await requester.post('/users/login').send(loginCredentials);
+
+    expect(response.status).to.be.equal(404);
+  });
+
+  it('iniciar sesión con contraseña inválido', async function () {
+
+    const loginCredentials = { email: 'matiasnicolasmoresi@gmail.com', password: '123' };
+    const response = await requester.post('/users/login').send(loginCredentials);
+
+    expect(response.status).to.be.equal(404);
+  });
+});
